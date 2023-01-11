0ff8b8d2f35955226ebea90553206608b2ed47dd
Kike-Alcaraz <ealcara2005@hotmail.com>
2023-01-12 00:45:58 +0100

    Subida inicial

---
+++ src/Hello.php
@@ -0,0 +1,20 @@
+<?php 
+
+/*
+ * (c) YOUR NAME <your@email.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+// If you don't to add a custom vendor folder, then use the simple class
+// namespace HelloComposer;
+namespace KikeAlcaraz\HelloComposer;
+
+class Hello
+{
+    public function say($toSay = "Hello, this is ICB0006-UF4-PR01")
+    {
+        return $toSay;
+    }
+}
